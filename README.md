237dd3810e5cd6a9243ad368fe8bbedb914e8dfb
marshall <marshallstutzman@gmail.com>
2023-08-02 14:37:32 -0400

    ready to go minus some testing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # errlog
 
 A simple api for creating error logs with zero dependencies outside of the Rust standard library.
+
+## Todo:
+- More unit tests
+- Extract private functions into modules for readability
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,17 @@
 //! the high-level convience needed from this tool.
 
 use std::error::Error;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::fs;
+use std::time::SystemTime;
 
 mod error;
 
 /// You must specify the file name within the path. In it's current state,
 /// only a single new directory can be created. If you are placing error logs
 /// outside of the root of the project, it's recommended to use an absolute
-/// file path.
+/// file path. Timestamps on error log are in relation to `UNIX_EPOCH`. This is
+/// a change for the future but will take *time* to implement. Get it?
 pub fn errlog(path: &str, error: String) -> Result<(), Box<dyn Error>> {
 
     let path = create_path_from_str(path)?;
@@ -41,8 +43,8 @@ fn create_path_from_str(text: &str) -> Result<PathBuf, Box<dyn Error>> {
     Ok(path)
 }
 
-fn check_or_make_directory(path: &PathBuf) -> Result<(), Box<dyn Error>> {
-    let mut dir_path = path.clone();
+fn check_or_make_directory(path: &Path) -> Result<(), Box<dyn Error>> {
+    let mut dir_path = path.to_path_buf();
     dir_path.pop();
 
     if dir_path.is_dir() {
@@ -53,8 +55,8 @@ fn check_or_make_directory(path: &PathBuf) -> Result<(), Box<dyn Error>> {
     Ok(())    
 }
 
-fn check_or_make_log(path: &PathBuf) -> Result<(), Box<dyn Error>> {
-    let path = path.clone();
+fn check_or_make_log(path: &Path) -> Result<(), Box<dyn Error>> {
+    let path = path.to_path_buf();
     if path.try_exists()? {
         Ok(())
     } else {
@@ -65,10 +67,16 @@ fn check_or_make_log(path: &PathBuf) -> Result<(), Box<dyn Error>> {
 
 fn append_log(file_path: &PathBuf, error: &str) -> Result<(), Box<dyn Error>> {
 
-    // create timestamp
-    // open file
-    // append timestamp & error
-    todo!();
+    let date_in_sec = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_secs();
+
+    let current_log = fs::read_to_string(file_path)?;
+
+    let updated_log = format!("{}\n{} - {}\n", current_log, date_in_sec, error);
+
+    fs::write(file_path, updated_log)?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -83,51 +91,93 @@ mod tests {
         assert_eq!("./output/test.log", path.to_str().unwrap().to_owned());
     }
 
+
     #[test]
     fn test_directory_checks() {
         let path = PathBuf::from("./test-data/test.log");
-        let result = check_or_make_directory(&path);
-        match result {
-            Ok(_) => assert!(true),
-            Err(e) => assert!(false, "Could not test for `./test-data` directory. Error: {e}"),
+        if let Err(e) =  check_or_make_directory(&path) {
+            assert!(false, "Could not test for `./test-data` directory. Error: {e}");
         }
 
         let mut path = PathBuf::from("./new-dir/test.log");
-        let result = check_or_make_directory(&path);
-        match result {
-            Ok(_) => assert!(true),
-            Err(e) => assert!(false, "Could not create `./new-dir/` directory. Error: {e}"),
+        if let Err(e) = check_or_make_directory(&path) {
+            assert!(false, "Could not create `./new-dir/` directory. Error: {e}");
         }
 
+        // cleanup
         path.pop();
         fs::remove_dir(&path).unwrap();
 
         let path = PathBuf::from("./test.log");
-        let result = check_or_make_directory(&path);
-        match result {
-            Ok(_) => assert!(true),
-            Err(e) => assert!(false, "Failed to check current directory. Error: {e}"),
+        if let Err(e) =  check_or_make_directory(&path) {
+            assert!(false, "Failed to check current directory. Error: {e}");
         }
+
+        assert!(true);
     }
+
 
     #[test]
     fn test_create_log() {
         let path = PathBuf::from("./test-data/new-file.txt");
-        let result = check_or_make_log(&path);
-        match result {
-            Ok(_) => assert!(true),
-            Err(e) => assert!(false, "Failed to create file `./test-data/new-file.txt`. Error: {e}"),
+        if let Err(e) = check_or_make_log(&path) {
+            assert!(false, "Failed to create file `./test-data/new-file.txt`. Error: {e}");
         }
 
+        // clean up
         fs::remove_file(&path).unwrap();
+
+        assert!(true);
     }
-    // #[test]
-    // fn errlog_test() {
-    //     let result = errlog("path", String::from("error"));
-    //     match result {
-    //         Ok(_) => assert!(true),
-    //         Err(_) => assert!(false),
-    //     }
-    //     assert!(false)
-    // }
+
+
+    #[test]
+    fn test_append() {
+        let path = PathBuf::from("./test-data/test.log");
+        if let Err(e) =  append_log(&path, "test error") {
+            assert!(false, "Could not write contents to `./test-data/test.log`. Error: {e}");
+        }
+
+        let bad_path = PathBuf::from("./test-data/does-not-exist.log");
+        if let Ok(_) =  append_log(&bad_path, "Something") {
+            assert!(false, "Should not be able to write contents to `./test-data/does-not-exist.log`.");
+        }
+
+        // clean up
+        fs::write("./test-data/test.log", "").unwrap();
+
+        assert!(true);
+    }
+
+
+    #[test]
+    fn errlog_success() {
+        if let Err(e) =  errlog("./test-data/errlog-unit-test.log", String::from("error log was successful")) {
+            assert!(false, "Something went horribly wrong. Error: {e}");
+        }
+
+        if let Err(e) = errlog("./new-folder/errlog-unit-test.log", String::from("error log was successful")) {
+            assert!(false, "Something went horribly wrong. Error: {e}");
+        }
+
+        // clean up
+        fs::remove_file("./new-folder/errlog-unit-test.log").unwrap();
+        fs::remove_dir("./new-folder").unwrap();
+
+        assert!(true);
+    }
+
+
+    #[test]
+    fn errlog_fail_bad_path() {
+        if let Ok(_) = errlog("./no-folder/abcd/errlog-unit-test.log", String::from("error log should fail")) {
+            assert!(false, "`./no-folder/abcd/errlog-unit-test.log` should have failed as a bad path.");
+        }
+
+        if let Ok(_) = errlog("./test-data/errlog-unit-test", String::from("error log should fail")) {
+            assert!(false, "not specifying a `.log` file should fail.");
+        }
+
+        assert!(true);
+    }
 }
